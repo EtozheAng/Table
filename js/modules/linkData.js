e59dfed56d3dc6415c927f1eb4efb734e2891f1e
EtozheAng <pavel.khisamutdinov@yandex.ru>
2023-07-19 13:05:59 +0300

    Updated database query links

--- js/modules/linkData.js
+++ js/modules/linkData.js
@@ -1,3 +1,5 @@
-export const urlOne = 'http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}'
+export const urlOne =
+  'https://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}'
 
-export const urlTwo = ' http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}'
+export const urlTwo =
+  ' https://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}'
